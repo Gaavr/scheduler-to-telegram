de6a431ce83d2ca8a33aa64896cf170ed79c4cb7
Gavrp <gavrplay@gmail.com>
2022-07-10 20:54:19 +0300

    initial commit

---
+++ src/main/java/com/gaavr/schedulertotelegram/SchedulerToTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.gaavr.schedulertotelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerToTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerToTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaavr/schedulertotelegram/SchedulerToTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaavr.schedulertotelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerToTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
